47c53d7c5398d31b43864c2922d17db1688ecfac
Seth Gibson <sethg@Seths-MacBook-Pro.local>
2017-06-16 14:35:56 -0500

    sql db setup

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devour)
+VALUES ('Avacado Burger', 0);
+
+INSERT INTO burgers (burger_name, devour)
+VALUES ('Jumbo Burger', 0);
+
+INSERT INTO burgers (burger_name, devour)
+VALUES ('Big Stack Burger', 0);
